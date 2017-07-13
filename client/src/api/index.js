fed50ae37c077cb6d44046c4719796284bb17a6e
voidSatisfaction <lourie@naver.com>
2017-07-13 20:24:28 +0900

    deploy: resolve speed issues

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,16 +1,10 @@
 import { api } from './base';
 
-function getCurrentLocationName({ lat, lng }) {
+function getNearEvents({ lat, lng }) {
   return api.get(`/myplace?lat=${lat}&lng=${lng}`)
     .then((res) => (res));
 }
 
-function getNearEvents(placeName) {
-  return api.get(`/myplace/events?place=${placeName}`)
-    .then((res) => (res));
-}
-
 export default {
-  getCurrentLocationName,
   getNearEvents,
 };
--- client/src/components/page/Home/index.jsx
+++ client/src/components/page/Home/index.jsx
@@ -97,7 +97,7 @@ export default class Home extends Component {
   }
 
   getUserNearEvents(location) {
-    return api.getCurrentLocationName(location)
+    return api.getNearEvents(location)
       .then((res) => res);
   }
 
